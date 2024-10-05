fb9e9c641e7b3fd4659792d60cdcac6e28b7fee3
Alex <alexfilsecker@gmail.com>
2024-10-05 15:00:04 +1000

    en vola?

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -13,19 +13,17 @@ const Experience = (): JSX.Element => {
       <div className="text-6xl w-min font-sans font-extrabold text-slate-300">
         Experience
       </div>
-      <div className="h-full flex gap-10 justify-center items-center pt-10">
-        <div className="flex flex-col gap-5 w-2/5 flex-grow">
-          {jobsInfo.map((jobInfo, index) => (
-            <ExperienceCard
-              key={index}
-              index={index}
-              setActiveCard={setActiveCard}
-              active={activeCard === index}
-              {...jobInfo}
-            />
-          ))}
-        </div>
-        <ExperienceDescription {...jobsInfo[activeCard].description} />
+      <div className="h-full flex gap-6 justify-center items-center pt-10">
+        {jobsInfo.map((jobInfo, index) => (
+          <ExperienceCard
+            key={index}
+            index={index}
+            setActiveCard={setActiveCard}
+            active={activeCard === index}
+            {...jobInfo}
+            description={jobInfo.description.sumary}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -4,6 +4,7 @@ type Props = JobInfoCard & {
   index: number;
   setActiveCard: (index: number) => void;
   active: boolean;
+  description: string;
 };
 
 const ExperienceCard = ({
@@ -14,28 +15,34 @@ const ExperienceCard = ({
   index,
   setActiveCard,
   active,
+  description,
 }: Props): JSX.Element => {
   const activeClass = `${active ? "border-emerald-400 bg-slate-800" : "border-slate-400"}`;
 
   return (
     <div
-      className={`bg-slate-900 
+      className={`bg-slate-900 w-1/4
         ${!active && "hover:border-emerald-700"} ${activeClass}
-        transition-all flex flex-col p-4 gap-2 rounded-2xl border-2 cursor-pointer`}
+        transition-all p-4 rounded-2xl border-2 cursor-pointer h-96`}
       onMouseDown={() => {
         setActiveCard(index);
       }}
     >
-      <div className="flex items-center justify-between gap-4">
-        <div className={`font-extrabold text-xl text-slate-200`}>
-          {jobTitle}
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+          <div className="text-slate-300 text-sm">{dateRange}</div>
+          <div className="flex flex-col gap 1">
+            <div className={`font-extrabold text-2xl text-slate-100`}>
+              {jobTitle}
+            </div>
+            <div className="flex gap-1 text-slate-300">
+              <div>{companyName}</div>
+              <div>|</div>
+              <div>{location}</div>
+            </div>
+          </div>
         </div>
-        <div className="text-slate-300">{dateRange}</div>
-      </div>
-      <div className="flex gap-1 text-slate-300">
-        <div>{companyName}</div>
-        <div>|</div>
-        <div>{location}</div>
+        <div className="text-slate-300 text-justify">{description}</div>
       </div>
     </div>
   );
