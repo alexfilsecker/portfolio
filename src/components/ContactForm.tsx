837d841baec4208d9d9ca33fe72df4ceb8d73f8d
Alex <alexfilsecker@gmail.com>
2024-10-22 00:03:32 +1100

    contact logic

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,26 +1,166 @@
-import { Mail, OpenInNew } from "@mui/icons-material";
-import { Button, TextField } from "@mui/material";
+import { Mail, Check } from "@mui/icons-material";
+import { Alert, Button, CircularProgress, TextField } from "@mui/material";
+import axios from "axios";
+import { useFormik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const ContactForm = (): JSX.Element => {
-  const handleClick = async () => {
-    const response = await fetch("api/mail");
-    const body = await response.json();
-    console.log(body);
-  };
+  const [formStatus, setFormStatus] = useState<
+    "idle" | "loading" | "success" | "error"
+  >("idle");
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    message: Yup.string().required("Message is required cannot be empty"),
+  });
+
+  const formik = useFormik({
+    initialValues: { name: "", lastName: "", email: "", message: "" },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      setFormStatus("loading");
+      try {
+        const response = await axios.post("/api/mail", values);
+        if (response.status === 200) {
+          setFormStatus("success");
+        } else {
+          console.error("status not 200");
+          setFormStatus("error");
+        }
+      } catch (e) {
+        console.error(e);
+        setFormStatus("error");
+      }
+    },
+  });
+
+  let bottomComponent: JSX.Element;
+  switch (formStatus) {
+    case "loading": {
+      bottomComponent = <CircularProgress className="self-center" />;
+      break;
+    }
+    case "success": {
+      bottomComponent = (
+        <Alert
+          icon={<Check fontSize="inherit" />}
+          severity="success"
+          onClose={() => {
+            setFormStatus("idle");
+          }}
+        >
+          Your message has been sent successfully. You should have recieved a
+          confirmation email
+        </Alert>
+      );
+      break;
+    }
+    case "error": {
+      bottomComponent = (
+        <Alert
+          severity="error"
+          onClose={() => {
+            setFormStatus("idle");
+          }}
+        >
+          Ups! something went wrong.
+        </Alert>
+      );
+      break;
+    }
+    case "idle":
+    default: {
+      const disabledByValues = Object.values(formik.values).reduce(
+        (prev, curr) => {
+          if (prev) return prev;
+          if (curr.length === 0) return true;
+          return false;
+        },
+        false
+      );
+      const disabledByErrors = Object.values(formik.errors).reduce(
+        (prev, current) => {
+          if (prev) return prev;
+          return Boolean(current);
+        },
+        false
+      );
+
+      bottomComponent = (
+        <Button
+          variant="outlined"
+          className="w-min self-center"
+          endIcon={<Mail />}
+          type="submit"
+          disabled={disabledByValues || disabledByErrors}
+        >
+          Send
+        </Button>
+      );
+      break;
+    }
+  }
 
   return (
-    <div className="flex flex-col gap-3 md:w-1/2">
+    <form
+      className="flex flex-col gap-3 md:w-1/2"
+      onSubmit={formik.handleSubmit}
+    >
       <div className="flex gap-3 w-full">
-        <TextField className="flex-grow" label="Name" variant="outlined" />
-        <TextField className="flex-grow" label="Last Name" variant="outlined" />
+        <TextField
+          name="name"
+          className="flex-grow"
+          label="Name"
+          variant="outlined"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+        <TextField
+          name="lastName"
+          className="flex-grow"
+          label="Last Name"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.lastName}
+          error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+          helperText={formik.touched.lastName && formik.errors.lastName}
+        />
       </div>
-      <TextField label="Email" variant="outlined" />
-      <TextField label="Message" variant="outlined" multiline rows={4} />
-      <button className="bg-transparent border-emerald-400 border-2 p-2 hover:bg-emerald-800 rounded-lg flex gap-2 text-lg md:text-xl transition-colors w-min self-center md:self-end">
-        <div className="text-slate-100">Send</div>
-        <Mail className="text-emerald-400 " />
-      </button>
-    </div>
+      <TextField
+        name="email"
+        label="Email"
+        variant="outlined"
+        required
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helperText={formik.touched.email && formik.errors.email}
+      />
+      <TextField
+        name="message"
+        label="Message"
+        variant="outlined"
+        multiline
+        rows={4}
+        required
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.message}
+        error={formik.touched.message && Boolean(formik.errors.message)}
+        helperText={formik.touched.message && formik.errors.message}
+      />
+
+      {bottomComponent}
+    </form>
   );
 };
 
--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,8 +1,54 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import Mailjet, { SendEmailV3_1 } from "node-mailjet";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("aldjñlasdjfñlasjdfñlasdfj");
-  res.status(200).json({ messaje: "hoooola" });
+const API_KEY = process.env.API_KEY;
+if (API_KEY === undefined) {
+  throw new Error("API KEY UNDEFINED");
+}
+const API_SECRET = process.env.API_SECRET;
+if (API_SECRET === undefined) {
+  throw new Error("API SECRET UNDEFINED");
+}
+
+const SENDER_EMAIL = "me@alexfilsecker.com";
+
+const mailjet = Mailjet.apiConnect(API_KEY, API_SECRET);
+
+const mailHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const response = await mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: SENDER_EMAIL,
+          Name: "MI PORTAFOLIO",
+        },
+        To: [
+          {
+            Email: "alexfilsecker@gmail.com",
+            Name: "YO",
+          },
+        ],
+        Subject: "TE ESCRIBIEROOOOOON",
+        TextPart: `${req.body.email}<${req.body.name} ${req.body.lastName}> Te ha enviado el mensaje: ${req.body.message}`,
+      },
+      {
+        From: {
+          Email: SENDER_EMAIL,
+          Name: "Alexander Filsecker",
+        },
+        To: [
+          {
+            Email: req.body.email,
+            Name: `${req.body.name} ${req.body.lastName}`,
+          },
+        ],
+        Subject: "Messaje sent to Alexander Filsecker",
+        TextPart: `You have sent me the following message:\n\n${req.body.message}\n\nExpect to be talking soon :)`,
+      },
+    ],
+  });
+
+  res.status(response.response.status).json(response.body);
 };
 
-export default handler;
+export default mailHandler;
