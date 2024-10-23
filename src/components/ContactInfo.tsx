5ae9569d0ea118353948f777c8565dbda3337b25
Alex <alexfilsecker@gmail.com>
2024-10-23 16:23:36 +1100

    contact info connected

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,5 +1,7 @@
 import { Email, GitHub, LinkedIn, Phone } from "@mui/icons-material";
+import { Tooltip, Button } from "@mui/material";
 import Link from "next/link";
+import React, { forwardRef, useState } from "react";
 
 type InfoProps = {
   name: string;
@@ -7,19 +9,24 @@ type InfoProps = {
   onClick?: () => void;
 };
 
-const Info = ({ name, icon, onClick }: InfoProps): JSX.Element => {
+const Info = forwardRef<HTMLDivElement, InfoProps>(function MyInfo(
+  { name, icon, onClick },
+  ref
+): JSX.Element {
   return (
     <div
       className="transition-colors w-min text-slate-50 py-1 px-4 text-nowrap flex gap-3 text-2xl hover:border-emerald-400 border-2 rounded-md border-transparent cursor-pointer items-center"
       onClick={onClick}
+      ref={ref}
     >
       {icon}
       <div>{name}</div>
     </div>
   );
-};
+});
 
 const ContactInfo = (): JSX.Element => {
+  const [copied, setCopied] = useState(false);
   return (
     <div className="flex flex-col gap-3 md:w-1/2">
       <Info
@@ -38,13 +45,20 @@ const ContactInfo = (): JSX.Element => {
           window.open("https://github.com/alexfilsecker");
         }}
       />
-      <Info
-        name="+61 405 241 772"
-        icon={<Phone fontSize="inherit" />}
-        onClick={() => {
-          console.log("añsldkja");
-        }}
-      />
+      <Tooltip title="Copied!" disableTouchListener open={copied} arrow>
+        <Info
+          name="+61 405 241 772"
+          icon={<Phone fontSize="inherit" />}
+          onClick={() => {
+            setCopied(true);
+            navigator.clipboard.writeText("+61 405 241 772");
+            const interval = setTimeout(() => {
+              setCopied(false);
+            }, 1000);
+          }}
+        />
+      </Tooltip>
+
       <Link href="mailto:alexfilsecker@gmail.com">
         <Info
           name="alexfilsecker@gmail.com"
